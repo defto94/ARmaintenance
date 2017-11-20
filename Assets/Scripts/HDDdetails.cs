5c7fa208b5f5a2c0e00a553e0c7a1f4e897fc037
Ola <defto94@gmail.com>
2017-11-20 22:22:14 +0100

    Target image and HDD model

---
+++ Assets/Scripts/HDDdetails.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class HDDdetails : MonoBehaviour {
+
+    public Text detailsText;
+    public string detailsString = "Rozmiar dysku:  931,51  GB";
+
+    public GameObject HDD;
+
+    // Use this for initialization
+    void Start () {
+        detailsText.text = detailsString;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
