a244f0e5655a30b2632d52827a82d500aa684d9a
Ola <defto94@gmail.com>
2017-11-30 23:09:27 +0100

    the text rotates to the camera

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    private GameObject rotatingObject;
+    public GameObject baseOfLabel;
+
+    private void Start()
+    {
+        rotatingObject = gameObject;
+        rotatingObject.transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward, baseOfLabel.transform.up);
+    }
+
+    private void Update()
+    {
+        rotatingObject.transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward, baseOfLabel.transform.up);
+    }
+}
