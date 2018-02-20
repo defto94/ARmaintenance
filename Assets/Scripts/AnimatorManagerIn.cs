869cdd225076c53cfba9cf9728283b38081598f0
Ola <defto94@gmail.com>
2018-02-20 23:43:21 +0100

    DVDdisk Second Animation added (Out)

---
+++ Assets/Scripts/AnimatorManagerIn.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatorManagerIn : MonoBehaviour
+{
+    private Animator modelAnimator;
+    private Animator trayAnimator;
+    private Animator pointerAnimator;
+    private Animator diskAnimator;
+    private GameObject buttonText;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        pointerAnimator = GameObject.Find("Pointer").GetComponent<Animator>();
+        trayAnimator = GameObject.Find("Tray").GetComponent<Animator>();
+        diskAnimator = GameObject.Find("DvdDisk2").GetComponent<Animator>();
+        buttonText = GameObject.Find("LaunchAnimationInButton");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NextAnimation()
+
+    {
+        buttonText.GetComponentInChildren<Text>().text = "Następny Krok";
+        pointerAnimator.Play("ArrowOut");
+
+        if (pointerAnimator.GetCurrentAnimatorStateInfo(0).IsName("ArrowOut"))
+        {
+            trayAnimator.Rebind();
+            pointerAnimator.Rebind();
+            diskAnimator.Rebind();
+            trayAnimator.Play("TrayOut");
+        }
+
+        if (trayAnimator.GetCurrentAnimatorStateInfo(0).IsName("TrayOut"))
+        {
+            trayAnimator.Rebind();
+            pointerAnimator.Rebind();
+            trayAnimator.Play("OnPlace");
+            diskAnimator.Play("DiskIn");
+        }
+
+        if (trayAnimator.GetCurrentAnimatorStateInfo(0).IsName("OnPlace"))
+        {
+            trayAnimator.Play("OnPlace");
+            pointerAnimator.Play("Arrow");
+            diskAnimator.Play("DiskOnPlace");
+        }
+
+        if (pointerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Arrow"))
+        {
+            trayAnimator.Play("TrayIn");
+            pointerAnimator.Rebind();
+            
+        }
+
+        if (trayAnimator.GetCurrentAnimatorStateInfo(0).IsName("TrayIn"))
+        {
+            trayAnimator.Rebind();
+            pointerAnimator.Rebind();
+            buttonText.GetComponentInChildren<Text>().text = "Włóż płytę";
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/AnimatorManagerOut.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatorManagerOut : MonoBehaviour {
+
+    private Animator modelAnimator;
+    private Animator trayAnimator;
+    private Animator pointerAnimator;
+    private Animator diskAnimator;
+    private GameObject buttonText;
+
+    void Start () {
+        pointerAnimator = GameObject.Find("Pointer").GetComponent<Animator>();
+        trayAnimator = GameObject.Find("Tray").GetComponent<Animator>();
+        diskAnimator = GameObject.Find("DvdDisk2").GetComponent<Animator>();
+        buttonText = GameObject.Find("LaunchAnimationOutButton");
+    }
+
+    public void NextAnimation()
+    {
+        pointerAnimator.Play("ArrowOut");
+        diskAnimator.Play("DiskOnPlace");
+        buttonText.GetComponentInChildren<Text>().text = "Następny Krok";
+
+        if (pointerAnimator.GetCurrentAnimatorStateInfo(0).IsName("ArrowOut"))
+        {
+            pointerAnimator.Rebind();
+            trayAnimator.Play("TrayOut");
+        }
+
+        if (trayAnimator.GetCurrentAnimatorStateInfo(0).IsName("TrayOut"))
+        {
+            trayAnimator.Rebind();
+            pointerAnimator.Rebind();
+            trayAnimator.Play("OnPlace");
+            diskAnimator.Play("DiskOut");
+        }
+
+        if (diskAnimator.GetCurrentAnimatorStateInfo(0).IsName("DiskOut"))
+        {
+            diskAnimator.Rebind();
+            pointerAnimator.Play("Arrow");
+        }
+
+        if (pointerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Arrow"))
+        {
+            pointerAnimator.Rebind();
+            diskAnimator.Rebind();
+            trayAnimator.Play("TrayIn");
+        }
+
+        if (trayAnimator.GetCurrentAnimatorStateInfo(0).IsName("TrayIn"))
+        {
+            pointerAnimator.Rebind();
+            diskAnimator.Rebind();
+            trayAnimator.Rebind();
+            buttonText.GetComponentInChildren<Text>().text = "Wyjmij płytę";
+        }
+    }
+}
